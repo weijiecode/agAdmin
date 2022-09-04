89bcc0c3a90ddb6358a00db956ace16ddeeb14f7
weijiecode <245619254@qq.com>
2022-09-04 23:15:16 +0800

    end

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="content">
-        <el-menu :default-active="typepage" class="el-menu-demo" mode="horizontal">
+        <el-menu @select="handleSelect" :default-active="typepage" class="el-menu-demo" mode="horizontal">
             <el-menu-item index="1">商品审核</el-menu-item>
             <el-menu-item index="2">用户管理</el-menu-item>
             <el-dropdown @command="outlogin" style="position: absolute;right: 15px;line-height: 61px;cursor: pointer;">
@@ -14,7 +14,7 @@
         </el-menu>
 
         <!-- 商品审核 -->
-        <div class="shopbox" v-if="typepage == 1">
+        <div class="shopbox" v-if="typepage == '1'">
             <!-- 商品管理列表 -->
             <el-table :data="shopData" border style="width: 100%;margin-top: 5px;">
                 <el-table-column prop="id" label="id" width="150">
@@ -40,6 +40,33 @@
             </el-table>
         </div>
 
+        <!-- 用户管理 -->
+        <div class="shopbox" v-if="typepage == '2'">
+            <!-- 用户管理列表 -->
+            <el-table :data="usersData" border style="width: 100%;margin-top: 5px;">
+                <el-table-column prop="id" label="id" width="150">
+                </el-table-column>
+                <el-table-column prop="username" label="用户名">
+                </el-table-column>
+                <el-table-column prop="nickname" label="昵称">
+                </el-table-column>
+                <el-table-column prop="sex" label="性别">
+                </el-table-column>
+                <el-table-column prop="phone" label="手机号">
+                </el-table-column>
+                <el-table-column prop="email" label="邮箱">
+                </el-table-column>
+                <el-table-column prop="introduction" label="个人简介">
+                </el-table-column>
+                <el-table-column label="操作" width="100">
+                    <template slot-scope="scope">
+                        <el-button @click="editHandleClick(scope.row)" type="text" size="small">编辑</el-button>
+                        <el-button @click="delHandleClick(scope.row)" type="text" size="small">删除</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
+
         <!-- 审核商品弹框 -->
         <el-dialog title="审核商品" :visible.sync="shopVisible" width="45%">
             <el-radio-group v-model="radio">
@@ -50,6 +77,39 @@
             <span slot="footer" class="dialog-footer">
                 <el-button @click="shopVisible = false">取 消</el-button>
                 <el-button type="primary" @click="saveshop">提交审核</el-button>
+            </span>
+        </el-dialog>
+
+        <!-- 修改用户信息弹框 -->
+        <el-dialog title="修改用户信息" :visible.sync="ushopVisible" width="60%">
+            <el-form status-icon label-width="100px" class="demo-ruleForm">
+                <el-form-item label="用户名">
+                    <el-input placeholder="请输入用户名" v-model="username"></el-input>
+                </el-form-item>
+                <el-form-item label="昵称">
+                    <el-input placeholder="请输入昵称" v-model="nickname">
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="性别">
+                    <el-radio v-model="sex" label="男">男</el-radio>
+                    <el-radio v-model="sex" label="女">女</el-radio>
+                </el-form-item>
+                <el-form-item label="电话">
+                    <el-input placeholder="请输入号码" v-model="phone">
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="邮箱">
+                    <el-input placeholder="请输入邮箱" v-model="email">
+                    </el-input>
+                </el-form-item>
+                <el-form-item label="简介">
+                    <el-input type="textarea" :rows="2" placeholder="请输入简介" v-model="introduction">
+                    </el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="shopVisible = false">取 消</el-button>
+                <el-button type="primary" @click="edituser">修改信息</el-button>
             </span>
         </el-dialog>
     </div>
@@ -77,24 +137,43 @@ export default {
             // 审核状态
             radio: 0,
             // 审核产品的id
-            id: ''
+            id: '',
+            // 用户数据
+            usersData: [],
+            // 用户id
+            userid: '',
+            // 用户名称
+            username: '',
+            // 用户昵称
+            nickname: '',
+            // 用户性别
+            sex: 0,
+            // 用户手机号
+            phone: '',
+            // 用户邮箱
+            email: '',
+            // 用户简介
+            introduction: '',
+            // 控制显示修改用户弹框
+            ushopVisible: false
         }
     },
     // 页面一加载执行下面方法
     mounted() {
         this.getShop()
+        this.getUsers()
     },
     methods: {
         // 点击审核
         handleClick(row) {
             this.shopVisible = true
             this.id = row.id
-            if(row.status == '待审核'){
+            if (row.status == '待审核') {
                 this.radio = 0
-            }else if(row.status == '审核通过'){
+            } else if (row.status == '审核通过') {
                 this.radio = 1
             }
-            else if (row.status == '审核未通过'){
+            else if (row.status == '审核未通过') {
                 this.radio = 2
             }
             console.log(row);
@@ -118,18 +197,92 @@ export default {
             }
             console.log(this.shopData)
         },
+        // 获取用户数据
+        async getUsers() {
+            const res = await this.$http.post("account/allusers")
+            console.log(res)
+            if (res.data.code === 200) {
+                this.usersData = res.data.data
+                this.usersData.forEach((item, index) => {
+                    if (item.sex === '0') {
+                        this.usersData[index].sex = '女'
+                    } else {
+                        this.usersData[index].sex = '男'
+                    }
+                })
+            }
+        },
+        // 切换菜单
+        handleSelect(item) {
+            console.log(item)
+            this.typepage = item
+        },
+        // 编辑用户
+        async editHandleClick(row) {
+            console.log(row)
+            this.ushopVisible = true
+            this.usersData.forEach(item => {
+                if (item.id == row.id) {
+                    this.userid = row.id
+                    this.username = item.username
+                    this.nickname = item.nickname
+                    this.phone = item.phone
+                    this.sex = item.sex
+                    this.email = item.email
+                    this.introduction = item.introduction
+                }
+            })
+
+        },
+        // 删除用户
+        async delHandleClick(row) {
+            console.log(row.id)
+            const res = await this.$http.post('account/deluser',{
+                id: row.id
+            })
+            if(res.data.code === 200) {
+                this.$message.success('删除成功')
+                this.getUsers()
+            }else {
+                this.$message.error('删除用户失败，请重试')
+            }
+        },
+        // 提交修改用户数据
+        async edituser() {
+            if(this.sex == '男') {
+                this.sex = '1'
+            }else {
+                this.sex = '0'
+            }
+            const res = await this.$http.post('account/edituser',{
+                username: this.username,
+                nickname: this.nickname,
+                sex: this.sex,
+                phone: this.phone,
+                email: this.email,
+                introduction:this.introduction,
+                id: this.userid
+            })
+            console.log(res)
+            if(res.data.code === 200) {
+                this.ushopVisible = false
+                this.$message.success('修改用户信息成功')
+                this.getUsers()
+            }
+            
+        },
         // 提交审核
         async saveshop() {
-            const res = await this.$http.post("shop/updatestatus",{
+            const res = await this.$http.post("shop/updatestatus", {
                 status: this.radio,
                 id: this.id
             })
             console.log(res)
-            if(res.data.code === 200) {
+            if (res.data.code === 200) {
                 this.shopVisible = false
                 this.$message.success("审核成功")
                 this.getShop()
-            }else {
+            } else {
                 this.$message.error("审核失败，请重试")
             }
         },
--- src/views/MerchantView.vue
+++ src/views/MerchantView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="content">
-        <el-menu :default-active="typepage" class="el-menu-demo" mode="horizontal">
+        <el-menu :default-active="typepage" @select="handleSelect" class="el-menu-demo" mode="horizontal">
             <el-menu-item index="1">商品管理</el-menu-item>
             <el-menu-item index="2">订单管理</el-menu-item>
             <el-dropdown @command="outlogin" style="position: absolute;right: 15px;line-height: 61px;cursor: pointer;">
@@ -12,7 +12,7 @@
                 </el-dropdown-menu>
             </el-dropdown>
         </el-menu>
-        <div class="shopbox" v-if="typepage == 1">
+        <div class="shopbox" v-if="typepage == '1'">
             <!-- 添加产品 -->
             <el-button @click="shopVisible = true" type="primary">添加产品</el-button>
             <!-- 商品管理列表 -->
@@ -46,7 +46,40 @@
             </el-table>
         </div>
         <!-- 订单管理列表 -->
-        <div class="shopbox" v-if="typepage == 2">
+        <div class="shopbox" v-if="typepage == '2'">
+            <!-- 订单管理列表 -->
+            <el-table :data="pay" border style="width: 100%;margin-top: 5px;">
+                <el-table-column prop="id" label="id">
+                </el-table-column>
+                <el-table-column prop="username" label="买家用户名">
+                </el-table-column>
+                <el-table-column prop="title" label="标题">
+                </el-table-column>
+                <el-table-column prop="price" label="价格">
+                </el-table-column>
+                <el-table-column prop="price" label="价格">
+                </el-table-column>
+                <el-table-column prop="photo" label="图片" width="150">
+                    <template slot-scope="scope">
+                        <img :src="scope.row.photo" class="shopimg">
+                    </template>
+                </el-table-column>
+                <el-table-column label="所属店铺">
+                    <template slot-scope="scope">
+                        {{ shopname }}
+                    </template>
+                </el-table-column>
+                <el-table-column prop="address" label="地址">
+                </el-table-column>
+                <el-table-column prop="remark" label="备注">
+                </el-table-column>
+                <el-table-column label="操作" width="150">
+                    <template slot-scope="scope">
+                        <el-button @click="payHandleClick(scope.row)" type="text" size="small">编辑</el-button>
+                        <el-button @click="delpay(scope.row)" type="text" size="small">取消订单</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
         </div>
         <!-- 添加商品弹框 -->
         <el-dialog title="添加商品" :visible.sync="shopVisible" width="60%">
@@ -101,8 +134,25 @@
                 </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
-                <el-button @click="shopVisible = false">取 消</el-button>
+                <el-button @click="ushopVisible = false">取 消</el-button>
                 <el-button type="primary" @click="usaveshop">修改产品</el-button>
+            </span>
+        </el-dialog>
+
+        <!-- 修改订单记录 -->
+        <el-dialog title="修改订单信息" :visible.sync="payVisible" width="60%">
+            <el-form status-icon label-width="100px" class="demo-ruleForm">
+                <el-form-item label="地址">
+                    <el-input placeholder="请输入地址" v-model="address"></el-input>
+                </el-form-item>
+                <el-form-item label="备注">
+                    <el-input type="textarea" :rows="2" placeholder="请输入备注" v-model="remark">
+                    </el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="payVisible = false">取 消</el-button>
+                <el-button type="primary" @click="editpay">修改信息</el-button>
             </span>
         </el-dialog>
     </div>
@@ -119,8 +169,10 @@ export default {
             shopData: [],
             // 店铺名称
             shopname: '',
-            // 控制添加商品弹框
+            // 控制添加商品弹框显示
             shopVisible: false,
+            // 控制修改弹框显示
+            payVisible: false,
             // 控制修改商品弹框
             ushopVisible: false,
             // 产品标题
@@ -140,7 +192,19 @@ export default {
             // 修改产品图片
             uphoto: '',
             // 指定编辑产品的id
-            shopid: ''
+            shopid: '',
+            // 购物车数据
+            shopCartData: [],
+            // 原始支付数据
+            oldpay: [],
+            // 筛选过滤后渲染的支付数据
+            pay: [],
+            // 所有用户名称和id
+            iduser: [],
+            // 地址
+            address: '',
+            // 备注
+            remark: ''
         }
     },
     // 页面一加载执行下面方法
@@ -196,6 +260,89 @@ export default {
                 })
             }
             console.log(this.shopData)
+
+
+            const res2 = await this.$http.post('shop/selectpay')
+            console.log(res2)
+            if(res2.data.code === 200){
+                this.oldpay = res2.data.data
+                // 获取用户名
+                this.$http.post('account/iduser').then(res1 => {
+                    console.log(res1)
+                    if(res2.data.code === 200){
+                    this.iduser = res1.data.data
+                    console.log(this.oldpay)
+                    this.pay = []
+                    this.oldpay.forEach((item,index) => {
+                        this.shopData.forEach(subitem => {
+                            if(item.commodityId == subitem.id){
+                                this.pay[index] = {
+                                    id: item.id,
+                                    title: subitem.title,
+                                    price: subitem.price,
+                                    photo: subitem.photo,
+                                    userId: item.userId,
+                                    remark: item.remark,
+                                    address: item.address
+                                }
+                            }
+                        })
+                    })
+                    console.log(this.pay)
+                    this.pay.forEach((subitem1,subindex1) => {
+                        this.iduser.forEach(subitem2 => {
+                            if(subitem1.userId == subitem2.id){
+                                this.pay[subindex1].username = subitem2.username
+                            }
+                        })
+                    })
+                    console.log(this.pay)
+                    }
+                })
+
+            }
+        },
+        // 保存修改订单信息
+        async editpay() {
+            const res = await this.$http.post('shop/updatepay',{
+                remark: this.remark,
+                address: this.address,
+                id: this.id
+            })
+            console.log(res)
+            if(res.data.code === 200) {
+                this.payVisible = false
+                this.$message.success('修改成功')
+                this.getShop()
+            }else {
+                this.$message.error('修改失败，请重试')
+            }
+        },
+        // 取消订单
+        delpay(row) {
+            this.$http.post('shop/delpay',{
+                id: row.id
+            }).then(res => {
+                console.log(res)
+                if(res.data.code === 200) {
+                    this.$message.success('该订单已取消')
+                    this.getShop()
+                }else {
+                    this.$message.error('订单取消失败，请重试')
+                }
+            })
+            
+        },
+        handleSelect(item) {
+            this.typepage = item
+        },
+        // 点击编辑按钮
+        payHandleClick(row) {
+            this.payVisible = true
+            this.id = row.id
+            this.address = row.address
+            this.remark = row.remark
+
         },
         // 退出登录
         outlogin() {
